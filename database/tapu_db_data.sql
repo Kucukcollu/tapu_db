1141a02d9cd6938bd0fe5a4e03f4064b58560dd8
Kucukcollu <kucukcollu@outlook.com>
2023-01-14 00:39:55 +0300

    tapu_db property table update.

--- database/tapu_db_data.sql
+++ database/tapu_db_data.sql
@@ -2,21 +2,57 @@
 
 ------------------------------------------------------------------------------------------------------------------
 -- datas of property table
-INSERT INTO property VALUES ('Apartment','Tom','Hanks',18034035,4000000,3,105,'New York,USA',223334444);
-INSERT INTO property VALUES ('Office','Jack','Nicholson',22054045,1350000,14,200,'London,England',333224444);
+--                           (pType, pOwnerName, pOwnerSurname, pOwnerID, pPrice, pRoomNumber, pSurvey, pAddress, pEstateID)
+INSERT INTO property VALUES ('Apartment','Tom','Hanks',18034035,4000000,3,105,'New York,USA',3209403233);
+INSERT INTO property VALUES ('Office','Jack','Nicholson',22054045,1350000,14,200,'London,England',2892636297);
+INSERT INTO property VALUES ('Apartment','Benedict','Cumberbatch',12054042,3002300,5,250,'Istanbul,Turkey',9284638711);
+INSERT INTO property VALUES ('Storehouse','Leonardo','DiCaprio',42054545,980000,NULL,1000,'London,England',2846304008);
+INSERT INTO property VALUES ('Office','Christian','Bale',25089404,769000,8,400,'Paris,France',3645938383);
+INSERT INTO property VALUES ('Storehouse','Natalie','Portman',14254054,450320,NULL,2500,'Tokyo,Japan',6732838470);
+INSERT INTO property VALUES ('Apartment','Scarlett','Johansson',09983462,1000000,3,180,'London,England',1524386944);
+INSERT INTO property VALUES ('Office','Megan','Fox',86654042,4360000,60,4590,'Tokyo,Japan',1439638466);
+INSERT INTO property VALUES ('Storehouse','Emma','Watson',12872350,5000000,NULL,430,'Berlin,Germany',1427532099);
+INSERT INTO property VALUES ('Apartment','Emilia','Clarke',19283422,4000000,5,400,'New York,USA',1523843222);
 ------------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------------
 -- datas of seller table
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
+INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
 INSERT INTO seller VALUES ('Tom','Hanks',16034035,'Apartment',223334444,6000000);
 ------------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------------
 -- datas of buyyer table
 INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
+INSERT INTO buyyer VALUES ('Jennifer','Lawrence',15245436,'Apartment',223334444,12000000);
 ------------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------------
 -- datas of sales table
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
+INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
 INSERT INTO sales VALUES (223334444,15245436,16034035,4000000,200000,40000,TRUE);
 ------------------------------------------------------------------------------------------------------------------
