4811c5b237ca53dd7ba154da3f82864440475d53
Kucukcollu <kucukcollu@outlook.com>
2023-01-14 15:03:26 +0300

    foreign key conflict fix.

--- database/tapu_db_schema.sql
+++ database/tapu_db_schema.sql
@@ -4,51 +4,51 @@
 
 -- DROP TABLE property;
 CREATE TABLE property (
-	pType    			varchar(15) not null, 
-	pOwnerName    	    varchar(20) not null,
-	pOwnerSurname       varchar(20) not null,
-	pOwnerID			int not null,
-	pPrice	            int not null,
-	pRoomNumber	        int,
-	pSurvey	            int not null,
+	pType    			varchar(25) not null, 
+	pOwnerName    	    varchar(30) not null,
+	pOwnerSurname       varchar(30) not null,
+	pOwnerID			bigint not null,
+	pPrice	            bigint not null,
+	pRoomNumber	        bigint,
+	pSurvey	            bigint not null,
 	pAddress            varchar(40) not null,
-	pEstateID           int not null,
-	primary key(pOwnerID, pEstateID)
+	pEstateID           bigint not null primary key
 );
 -------------------------------------------------------
 
 -- DROP TABLE seller;
 CREATE TABLE seller(
-	sName    	    	varchar(20) not null,
-	sSurname       		varchar(20) not null,
-	sID					int not null,
-	pType    			varchar(15) not null, 
-	pOwnerID			int not null,
-	sBudget				int not null,
+	sName    	    	varchar(30) not null,
+	sSurname       		varchar(30) not null,
+	sID					bigint not null,
+	pType    			varchar(25) not null, 
+	pOwnerID			bigint not null,
+	sBudget				bigint not null,
 	primary key(sID, pOwnerID)
 );
 -------------------------------------------------------
 
 -- DROP TABLE buyyer;
 CREATE TABLE buyyer(
-	bName    	    	varchar(20) not null,
-	bSurname       		varchar(20) not null,
-	bID					int not null,
-	pType    			varchar(15) not null, 
-	pOwnerID			int not null,
-	bBudget				int not null,
+	bName    	    	varchar(30) not null,
+	bSurname       		varchar(30) not null,
+	bID					bigint not null,
+	pType    			varchar(25) not null, 
+	pOwnerID			bigint not null,
+	bBudget				bigint not null,
 	primary key(bID, pOwnerID)
 );
 -------------------------------------------------------
 
 -- DROP TABLE sales;
 CREATE TABLE sales(
-	pEstateID           int not null,
-	bID					int not null,
-	sID					int not null,
-	pPrice	            int not null,
-	sComission			int not null,
-	sCirCap 			int not null,			
-	sIsDonation	        boolean not null
+	bID					bigint not null,
+	sID					bigint not null,
+	pPrice	            bigint not null,
+	sComission			bigint not null,
+	sCirCap 			bigint not null,			
+	sIsDonation	        boolean not null,
+	pEstateID           bigint references property(pEstateID),
+	UNIQUE(pEstateID)
 );
 -------------------------------------------------------
