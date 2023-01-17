50e69ac19b0ef4dcf1ef95686de9a9d0fdbc840a
Kucukcollu <kucukcollu@outlook.com>
2023-01-18 00:32:30 +0300

    views add.

--- database/tapu_db_queries.sql
+++ database/tapu_db_queries.sql
@@ -13,3 +13,52 @@ FROM buyyer
 SELECT *
 FROM sales
 --------------------------------------
+CREATE VIEW istanbul_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='Istanbul,Turkey'
+--------------------------------------
+CREATE VIEW tokyo_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='Tokyo,Japan'
+--------------------------------------
+CREATE VIEW newyork_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='New York,USA'
+--------------------------------------
+CREATE VIEW london_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='London,England'
+--------------------------------------
+CREATE VIEW paris_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='Paris,France'
+--------------------------------------
+CREATE VIEW san_francisco_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='San Francisco,USA'
+--------------------------------------
+CREATE VIEW seatle_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='Seatle,USA'
+--------------------------------------
+CREATE VIEW paris_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='Paris,France'
+--------------------------------------
+CREATE VIEW berlin_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='Berlin,Germany'
+--------------------------------------
+CREATE VIEW san_diago_places_money AS 
+SELECT property.pprice 
+FROM property 
+WHERE property.paddress='San Diego,USA'
